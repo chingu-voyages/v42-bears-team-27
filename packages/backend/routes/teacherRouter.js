7b255714327c69ef1bf858925e2e240244cefd18
Henok Hailemariam <henokhm2@gmail.com>
2023-01-16 23:46:35 +0300

    feat(teacher-validator): add teacher validator
    Add teacher validator middleware for incoming JSON data using express-validator package

--- packages/backend/routes/teacherRouter.js
+++ packages/backend/routes/teacherRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
-router.post('/create', function (req, res, _) {
+const { validateTeacher } = require('../validators/teacherValidator');
+
+router.route('/create').post(validateTeacher, function (req, res, _) {
   const data = req.body;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ dataRecieved: data }));
