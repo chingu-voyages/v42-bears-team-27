a2215e092cdcb3fe00f5047a1aef4d5a77d84681
Tarwat <t.uddin9121@gmail.com>
2023-01-21 12:56:01 +0000

    feat(index): update home page
    Added hero image and headline

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,11 +1,44 @@
 import type { ReactElement } from 'react';
+import Image from 'next/image';
 
 import Layout from '../layouts/Layout';
 
-const Home = () => <div>Home</div>;
+import heroImg from '../../public/hero.jpg';
+
+const Home = () => (
+  <div sx={{ pt: 5, px: 2 }}>
+    <div sx={{ textAlign: 'center', mb: 4 }}>
+      <Image
+        src={heroImg}
+        alt="Boy working on school work with laptop and headphones"
+        width={560}
+        height={352}
+      />
+    </div>
+    <h2
+      sx={{
+        variant: 'text.h3',
+        width: 720,
+        mx: 'auto',
+        color: 'primary',
+        textAlign: 'center',
+      }}
+    >
+      An All-In-One Inclusive Learning Platform Built For Both Students and
+      Teachers
+    </h2>
+  </div>
+);
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return <Layout title="RemoteClass">{page}</Layout>;
+  return (
+    <Layout
+      title="RemoteClass"
+      description="An all-in-one inclusive learning platform built for both students and teachers"
+    >
+      {page}
+    </Layout>
+  );
 };
 
 export default Home;
