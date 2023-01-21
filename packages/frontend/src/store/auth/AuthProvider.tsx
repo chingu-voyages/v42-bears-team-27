1c847596737cad5a95a8ba8bb77769b3932de020
Tarwat <t.uddin9121@gmail.com>
2023-01-21 15:32:48 +0000

    feat(auth): update context & login/logout handlers of store

--- packages/frontend/src/store/auth/AuthProvider.tsx
+++ packages/frontend/src/store/auth/AuthProvider.tsx
@@ -1,39 +1,60 @@
 import { useState, useMemo } from 'react';
 
-import type {
-  ITeacherCredentials,
-  IStudentCredentials,
-} from '../../interfaces';
-import { AuthContext, type IAuthContext } from './AuthContext';
-
-type UserCredentials = ITeacherCredentials | IStudentCredentials;
+import type { IUserCredentials, IUserData } from '../../interfaces';
+import { AuthContext, type IAuthContext } from './auth-context';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const AuthProvider: React.FC<Props> = ({ children }) => {
-  const [user, setUser] = useState<UserCredentials | null>(null);
+  const [user, setUser] = useState<IUserData | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [jsonToken, setJSONToken] = useState<string | null>(null);
 
-  const loginHandler = (userCredentials: UserCredentials) => {
-    setUser(userCredentials);
+  const loginHandler = async (
+    userCredentials: IUserCredentials,
+    userRole: 'student' | 'teacher',
+  ) => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/${userRole}/login`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userCredentials),
+      },
+    );
+
+    if (!res.ok) {
+      return 'Error: Failed to login!';
+    }
+
+    const { email, token }: any = await res.json();
+
+    setUser({ role: userRole, email });
     setIsLoggedIn(true);
+    setJSONToken(token);
+
+    return 'Success: Logged in!';
   };
 
   const logoutHandler = () => {
     setUser(null);
     setIsLoggedIn(false);
+    setJSONToken(null);
   };
 
   const authContext: IAuthContext = useMemo(
     () => ({
       user,
       isLoggedIn,
+      jsonToken,
       onLogin: loginHandler,
       onLogout: logoutHandler,
     }),
-    [user, isLoggedIn],
+    [user, isLoggedIn, jsonToken],
   );
 
   return (
