91eff0f169b9319231090206e5288fbad9c2b5eb
Tarwat <t.uddin9121@gmail.com>
2023-01-21 18:25:36 +0000

    feat(home): add home page for entry route of web app

---
+++ packages/frontend/src/components/home/MainHeader/MainHeader.tsx
@@ -0,0 +1,62 @@
+import { Modal } from '../../UI';
+import Login from './Login';
+
+const MainHeader: React.FC = () => (
+  <header
+    sx={{
+      py: 3,
+      px: 4,
+      bg: 'muted',
+    }}
+  >
+    <nav
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <h1
+        sx={{
+          variant: 'text.h2',
+          m: 0,
+          textAlign: ['center', null, 'start'],
+        }}
+      >
+        RemoteClass
+      </h1>
+      <div sx={{ display: 'flex', columnGap: 3 }}>
+        <Modal title="Login">
+          <Login />
+        </Modal>
+        <a
+          sx={{
+            variant: 'text.label',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            columnGap: 10,
+            height: 48,
+            py: 2,
+            px: 4,
+            fontSize: 18,
+            textDecoration: 'none',
+            bg: 'white',
+            color: 'primary',
+            border: '2px solid currentColor ',
+            borderRadius: 50,
+            cursor: 'pointer',
+            '&:hover': {
+              bg: '#e6e6e6',
+            },
+          }}
+          href="/signup"
+        >
+          Sign up
+        </a>
+      </div>
+    </nav>
+  </header>
+);
+
+export default MainHeader;
