657ebab080ba983f7784b040e3210a841e452d26
Tarwat <t.uddin9121@gmail.com>
2023-01-21 15:30:04 +0000

    feat(interface): add IUserCredentials & IUserData interfaces

--- packages/frontend/src/interfaces.ts
+++ packages/frontend/src/interfaces.ts
@@ -1,9 +1,21 @@
+// TODO: remove all instances of type import + interface itself
 export interface ITeacherCredentials {
   username: string;
   password: string;
 }
-
+// TODO: remove all instances of type import + interface itself
 export interface IStudentCredentials {
   email: string;
   password: string;
+}
+
+export interface IUserCredentials {
+  email: string;
+  password: string;
+}
+
+export interface IUserData {
+  role: 'student' | 'teacher';
+  email: string;
+  [key: string]: any;
 }
