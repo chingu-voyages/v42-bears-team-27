973e2c9b7870a2ca64f53c55381f52c5c05e6dc9
Tarwat <t.uddin9121@gmail.com>
2023-01-22 22:05:49 +0000

    refactor: update/remove interfaces

--- packages/frontend/src/interfaces.ts
+++ packages/frontend/src/interfaces.ts
@@ -1,14 +1,3 @@
-// TODO: remove all instances of type import + interface itself
-export interface ITeacherCredentials {
-  username: string;
-  password: string;
-}
-// TODO: remove all instances of type import + interface itself
-export interface IStudentCredentials {
-  email: string;
-  password: string;
-}
-
 export interface IUserCredentials {
   email: string;
   password: string;
@@ -16,6 +5,5 @@ export interface IUserCredentials {
 
 export interface IUserData {
   role: 'student' | 'teacher';
-  email: string;
   [key: string]: any;
 }
