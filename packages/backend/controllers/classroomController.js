fdf11bca330c2d8d5c3bb167206e0422ba24770c
Henok Hailemariam <henokhm2@gmail.com>
2023-01-25 00:13:57 +0300

    fix(classroomcontroller): add return keyword so that sending response exits function

--- packages/backend/controllers/classroomController.js
+++ packages/backend/controllers/classroomController.js
@@ -3,21 +3,23 @@ const Classroom = require('../models/classroomModel');
 const getClassroom = async (req, res) => {
   const classroom = await Classroom.findById(req.params.id);
   if (!classroom) {
-    res.status(404).json({ error: 'Classroom not found' });
+    return res.status(404).json({ error: 'Classroom not found' });
   }
   if (
     !classroom.teacher.equals(res.locals.user.id) ||
     !classroom.students.some((student) => student.equals(res.locals.user.id))
   ) {
-    res.status(401).json({ error: 'Unauthorized access' });
+    return res.status(401).json({ error: 'Unauthorized access' });
   }
-  res.json(classroom);
+  return res.json(classroom);
 };
 
 const addClassroom = async (req, res) => {
   const classroom = await Classroom.findOne({ teacher: res.locals.user.id });
   if (classroom) {
-    res.status(400).json({ error: 'Teacher already belongs to a classroom' });
+    return res
+      .status(400)
+      .json({ error: 'Teacher already belongs to a classroom' });
   }
   const newClassroom = new Classroom({
     name: req.body.name,
@@ -26,34 +28,34 @@ const addClassroom = async (req, res) => {
     subjects: req.body.subjects,
   });
   await newClassroom.save();
-  res.json(newClassroom);
+  return res.json(newClassroom);
 };
 
 const updateClassroom = async (req, res) => {
   const classroom = await Classroom.findById(req.params.id);
   if (!classroom) {
-    res.status(404).json({ error: 'Classroom not found' });
+    return res.status(404).json({ error: 'Classroom not found' });
   }
   if (!classroom.teacher.equals(res.locals.user.id)) {
-    res.status(401).json({ error: 'Unauthorized access' });
+    return res.status(401).json({ error: 'Unauthorized access' });
   }
   classroom.name = req.body.name;
   classroom.students = req.body.students;
   classroom.subjects = req.body.subjects;
   await classroom.save();
-  res.json(classroom);
+  return res.json(classroom);
 };
 
 const deleteClassroom = async (req, res) => {
   const classroom = await Classroom.findById(req.params.id);
   if (!classroom) {
-    res.status(404).json({ error: 'Classroom not found' });
+    return res.status(404).json({ error: 'Classroom not found' });
   }
   if (!classroom.teacher.equals(res.locals.user.id)) {
-    res.status(401).json({ error: 'Unauthorized access' });
+    return res.status(401).json({ error: 'Unauthorized access' });
   }
   await classroom.remove();
-  res.json({ message: 'Classroom deleted' });
+  return res.json({ message: 'Classroom deleted' });
 };
 
 module.exports = {
