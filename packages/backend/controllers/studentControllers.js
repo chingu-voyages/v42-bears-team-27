d2f285d6739124fd60ef90e84656935798168ed9
Eloi-Perez <37666229+Eloi-Perez@users.noreply.github.com>
2023-01-24 17:03:28 +0000

    Feat/add nodemailer (#103)
    * feat: add nodemailer
    
    * feat: send email function
    
    * feat: html template and email controller
    
    * feat: send email only in production
    
    Co-authored-by: Henok Hailemariam <henokhm2@gmail.com>

--- packages/backend/controllers/studentControllers.js
+++ packages/backend/controllers/studentControllers.js
@@ -1,5 +1,6 @@
+/* eslint spaced-comment: 0 */
 const Student = require('../models/studentModel');
-const { generatePassword, generateJWT } = require('../utils');
+const { generatePassword, generateJWT, sendEmail } = require('../utils');
 
 const createStudent = async (req, res) => {
   /*  fullName: 'LastName, FirstName',
@@ -21,15 +22,35 @@ const createStudent = async (req, res) => {
         password: hashedPassword,
         classroom,
       })
-        .then(() =>
-          // TODO? send email to student with the password
-          res.status(201).json({
-            message: 'Created Successfully',
-            fullName,
-            // TODO remove once is sent by email
-            password,
-          }),
-        )
+        .then(() => {
+          if (process.env.NODE_ENV === 'production') {
+            // send email to student with the password
+            const content = () => /*html*/ `You have been registered into
+            Remote Class! Please use your email and this password to
+            login.<br>Password: ${password}`;
+            const emailWrap = {
+              to: email,
+              subject: 'Welcome to Remote Class',
+              message: content(),
+            };
+            sendEmail(emailWrap)
+              .then(() =>
+                res.status(201).json({
+                  message: 'Created Successfully',
+                  fullName,
+                }),
+              )
+              .catch((err) => res.status(400).json({ message: err }));
+          } else {
+            // development code:
+            return res.status(201).json({
+              message: 'Created Successfully',
+              fullName,
+              password,
+            });
+          }
+          return false;
+        })
         .catch((err) => res.status(400).json({ message: err }));
       return false;
     })
