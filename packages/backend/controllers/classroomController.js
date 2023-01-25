0d0db8966fc0df2d3341bbcdf98821cf590ecd92
Henok Hailemariam <henokhm2@gmail.com>
2023-01-25 23:17:15 +0300

    feat(classroomcontroller): add broadcast message function to classroomController

--- packages/backend/controllers/classroomController.js
+++ packages/backend/controllers/classroomController.js
@@ -1,4 +1,6 @@
 const Classroom = require('../models/classroomModel');
+const Student = require('../models/studentModel');
+const Message = require('../models/messageModel');
 
 const getClassroom = async (req, res) => {
   const classroom = await Classroom.findById(req.params.id);
@@ -58,9 +60,47 @@ const deleteClassroom = async (req, res) => {
   return res.json({ message: 'Classroom deleted' });
 };
 
+const broadcastMessage = async (req, res) => {
+  const { messageHeader, messageBody } = req.body;
+
+  const teacherID = res.locals.user;
+
+  try {
+    const { students } = await Classroom.findOne({ teacher: teacherID });
+
+    if (!students.length) {
+      return res
+        .status(400)
+        .json({ message: 'You have no students in your classroom' });
+    }
+
+    const newMessage = await Message.create({
+      isBroadcast: true,
+      fromTeacher: teacherID,
+      messageHeader,
+      messageBody,
+    });
+
+    students.forEach(async (studentID) => {
+      const student = await Student.findById(studentID);
+
+      student.inbox.push({
+        messageID: newMessage._id,
+        hasBeenRead: false,
+      });
+      await student.save();
+    });
+    return res.status(200).json({ message: 'message sent!' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   deleteClassroom,
   updateClassroom,
   getClassroom,
   addClassroom,
+  broadcastMessage,
 };
