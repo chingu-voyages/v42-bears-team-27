8c9619b4c5f695e3429d07bd4fc501579ea3cb2c
Tarwat <t.uddin9121@gmail.com>
2023-01-25 20:31:58 +0000

    feat: add new interfaces

--- packages/frontend/src/interfaces.ts
+++ packages/frontend/src/interfaces.ts
@@ -7,3 +7,22 @@ export interface IUserData {
   role: 'student' | 'teacher';
   [key: string]: any;
 }
+
+export interface ITask {
+  id: string | number;
+  type: 'lesson' | 'exercise' | 'test';
+  subject: string;
+  topic: string;
+}
+
+export interface IEvent {
+  type: 'one-time' | 'schedule';
+  createdAt: Date;
+  dueDate: Date;
+  tasks: ITask[];
+}
+
+export interface IClassroomData {
+  events: IEvent[];
+  [key: string]: any;
+}
