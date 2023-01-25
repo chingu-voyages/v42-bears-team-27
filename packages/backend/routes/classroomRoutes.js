d8437750169f1f97a4abbfb9f9a2fdb63dd34da7
Henok Hailemariam <henokhm2@gmail.com>
2023-01-25 23:18:29 +0300

    feat(classroomroutes): add broadcast-message route

--- packages/backend/routes/classroomRoutes.js
+++ packages/backend/routes/classroomRoutes.js
@@ -5,13 +5,23 @@ const {
   updateClassroom,
   getClassroom,
   addClassroom,
+  broadcastMessage,
 } = require('../controllers/classroomController');
 
-const { checkTeacherAuthenticated } = require('../middlewares');
+const {
+  checkTeacherAuthenticated,
+  validateMessage,
+} = require('../middlewares');
 
 router.post('/create', checkTeacherAuthenticated, addClassroom);
 router.get('/:id', checkTeacherAuthenticated, getClassroom);
 router.delete('/:id', checkTeacherAuthenticated, deleteClassroom);
 router.put('/:id', checkTeacherAuthenticated, updateClassroom);
+router.post(
+  '/broadcast-message',
+  checkTeacherAuthenticated,
+  validateMessage,
+  broadcastMessage,
+);
 
 module.exports = router;
