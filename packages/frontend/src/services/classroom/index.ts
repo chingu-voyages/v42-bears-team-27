8594ba9ece1332db443b8f389c03ee4202dc7693
Tarwat <t.uddin9121@gmail.com>
2023-01-28 15:02:44 +0000

    feat(services): add postClassroomEvent and putClassroomEvent handlers
    Added handlers are used for updating and creating new classroom events

--- packages/frontend/src/services/classroom/index.ts
+++ packages/frontend/src/services/classroom/index.ts
@@ -20,14 +20,39 @@ export const getClassroomEvents: Fetcher<IEvent[]> = (endpoint: string) =>
     },
   }).then((res) => res.json());
 
+// POST REQUESTS
+export const postClassroomEvent = (newEvent: IEvent) => {
+  fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/classroom`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newEvent),
+  }).then((res) => res.json());
+};
+
 // PUT REQUESTS
-export const putClassroom = (newEvents: Partial<IClassroom>) => {
+export const putClassroom = (
+  updatedClassroom: Omit<IClassroom, 'events' | 'subjects'>,
+) => {
   fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/classroom`, {
     method: 'PUT',
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(newEvents),
+    body: JSON.stringify(updatedClassroom),
+  }).then((res) => res.json());
+};
+
+export const putClassroomEvent = (updatedEvent: IEvent) => {
+  fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/classroom`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedEvent),
   }).then((res) => res.json());
 };
