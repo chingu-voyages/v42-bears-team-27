22b5f8eb2600f70f228f24f8c28ed5ab8b50b9dc
Tarwat <t.uddin9121@gmail.com>
2023-01-30 20:35:21 +0000

    chore: update index

--- packages/frontend/src/components/ui/index.ts
+++ packages/frontend/src/components/ui/index.ts
@@ -3,3 +3,6 @@ export * from './TextField';
 export * from './Calendar';
 export * from './IconButton';
 export * from './Modal';
+export * from './Avatar';
+export * from './Checkbox';
+export * from './Menu';
