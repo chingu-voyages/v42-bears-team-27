0e45cdb733909602923b8684a14322b963b36138
Tarwat <t.uddin9121@gmail.com>
2023-01-31 22:58:13 +0000

    feat(text-field): add TextFieldArea component
    Component used to render a textarea element instead of an input element

--- packages/frontend/src/components/ui/TextField/TextField.tsx
+++ packages/frontend/src/components/ui/TextField/TextField.tsx
@@ -7,8 +7,24 @@ import { forwardRef } from 'react';
 import type { ThemeUIStyleObject } from 'theme-ui';
 import * as LabelPrimitive from '@radix-ui/react-label';
 
-export interface TextFieldProps
-  extends HTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
+const baseStyles: ThemeUIStyleObject = {
+  variant: 'text.label',
+  width: 'inherit',
+  height: 'inherit',
+  py: 3,
+  pl: 26,
+  pr: 4,
+  fontFamily: 'heading',
+  fontSize: 14,
+  color: 'primary',
+  border: '2px solid',
+  borderColor: 'gray',
+  '&:placeholder-shown': {
+    color: 'gray',
+  },
+};
+
+export interface TextFieldProps extends HTMLAttributes<HTMLInputElement> {
   /**
    * Label for the component
    */
@@ -18,91 +34,79 @@ export interface TextFieldProps
    * @default 'text'
    */
   type?: 'text' | 'email' | 'password';
-  /**
-   * Enable multiline for the component
-   * @default false
-   */
-  multiline?: boolean;
   sx?: ThemeUIStyleObject;
   value?: string;
   required?: boolean;
 }
 
 export const TextField: ForwardRefExoticComponent<
-TextFieldProps & RefAttributes<any>
+TextFieldProps & RefAttributes<HTMLInputElement>
 > = forwardRef(
-  (
-    {
-      key,
-      id,
-      label,
-      type = 'text',
-      multiline = false,
-      required = false,
-      sx,
-      ...rest
-    },
-    ref,
-  ) => {
-    const styles: ThemeUIStyleObject = {
-      variant: 'text.label',
-      width: 'inherit',
-      height: 'inherit',
-      py: 3,
-      pl: 26,
-      pr: 4,
-      fontFamily: 'heading',
-      fontSize: 14,
-      color: 'primary',
-      border: '2px solid',
-      borderColor: 'gray',
-      '&:placeholder-shown': {
-        color: 'gray',
-      },
-    };
-
-    return (
-      <div
-        key={key}
-        sx={{ display: 'flex', flexDirection: 'column', rowGap: 3, ...sx }}
+  ({ key, id, label, type = 'text', required = false, sx, ...rest }, ref) => (
+    <div
+      key={key}
+      sx={{ display: 'flex', flexDirection: 'column', rowGap: 3, ...sx }}
+    >
+      <LabelPrimitive.Root
+        htmlFor={id}
+        sx={{ variant: 'text.label', fontFamily: 'heading', fontSize: 18 }}
       >
-        <LabelPrimitive.Root
-          htmlFor={id}
-          sx={{ variant: 'text.label', fontFamily: 'heading', fontSize: 18 }}
-        >
-          {label}
-        </LabelPrimitive.Root>
-        <div
-          sx={{
-            width: '100%',
-            height: multiline ? 174 : 50,
-            bg: 'background',
-          }}
-        >
-          {multiline ? (
-            <textarea
-              ref={ref}
-              id={id}
-              required={required}
-              sx={styles}
-              {...rest}
-            />
-          ) : (
-            <input
-              ref={ref}
-              id={id}
-              type={type}
-              required={required}
-              sx={styles}
-              {...rest}
-            />
-          )}
-        </div>
+        {label}
+      </LabelPrimitive.Root>
+      <div sx={{ width: '100%', height: 50, bg: 'background' }}>
+        <input
+          ref={ref}
+          id={id}
+          type={type}
+          required={required}
+          sx={baseStyles}
+          {...rest}
+        />
       </div>
-    );
-  },
+    </div>
+  ),
 );
 
 if (process.env.NODE_ENV !== 'production') {
   TextField.displayName = 'TextField';
+}
+
+export interface TextFieldAreaProps
+  extends HTMLAttributes<HTMLTextAreaElement> {
+  /**
+   * Label for the component
+   */
+  label: string;
+  sx?: ThemeUIStyleObject;
+  value?: string;
+  required?: boolean;
+}
+
+export const TextFieldArea: ForwardRefExoticComponent<
+TextFieldAreaProps & RefAttributes<HTMLTextAreaElement>
+> = forwardRef(({ key, id, label, required = false, sx, ...rest }, ref) => (
+  <div
+    key={key}
+    sx={{ display: 'flex', flexDirection: 'column', rowGap: 3, ...sx }}
+  >
+    <LabelPrimitive.Root
+      htmlFor={id}
+      sx={{ variant: 'text.label', fontFamily: 'heading', fontSize: 18 }}
+    >
+      {label}
+    </LabelPrimitive.Root>
+    <div sx={{ width: '100%', height: 174, bg: 'background' }}>
+      <textarea
+        ref={ref}
+        id={id}
+        required={required}
+        sx={baseStyles}
+        {...rest}
+      />
+    </div>
+  </div>
+));
+
+if (process.env.NODE_ENV !== 'production') {
+  TextFieldArea.displayName = 'TextFieldArea';
 }
