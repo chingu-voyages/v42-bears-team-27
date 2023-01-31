999640b0588bdbd0a724225797e279a4686d6ec9
Tarwat <t.uddin9121@gmail.com>
2023-01-31 23:50:35 +0000

    docs(calendars): add comment to include StudentCalendar component as an export

--- packages/frontend/src/components/dashboard/home/calendars/index.ts
+++ packages/frontend/src/components/dashboard/home/calendars/index.ts
@@ -1,2 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as TeacherCalendar } from './TeacherCalendar';
+// TODO: add StudentCalendar component
+// export { default as StudentCalendar } from './StudentCalendar';
