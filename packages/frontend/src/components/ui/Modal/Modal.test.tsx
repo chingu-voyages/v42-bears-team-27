ea7410930029e227930c1a587d682612ec97441e
Tarwat <t.uddin9121@gmail.com>
2023-01-31 23:08:19 +0000

    test(modal): update and add new snapshots

--- packages/frontend/src/components/ui/Modal/Modal.test.tsx
+++ packages/frontend/src/components/ui/Modal/Modal.test.tsx
@@ -1,13 +1,18 @@
 import { render, screen } from 'test-utils';
 import userEvent from '@testing-library/user-event';
 
-import { Modal } from './Modal';
+import { Dialog, DialogContent, DialogTrigger } from './Modal';
+import { Button } from '../Button';
 
 describe('<Modal />', () => {
   describe('Snapshots', () => {
     it('should match snapshot', () => {
       const { container } = render(
-        <Modal title="Foo" width={320} height={480} />,
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Foo</Button>
+          </DialogTrigger>
+        </Dialog>,
       );
       expect(container).toMatchSnapshot();
     });
@@ -16,19 +21,14 @@ describe('<Modal />', () => {
   describe('Modal state', () => {
     it('should display Modal children when state open', async () => {
       render(
-        <Modal title="Foo" width={320} height={480}>
-          Hello, World
-        </Modal>,
+        <Dialog open>
+          <DialogContent title="Foo" width={320} height={480}>
+            Hello, World
+          </DialogContent>
+        </Dialog>,
       );
       await userEvent.click(screen.getByRole('button'));
       expect(screen.getByText(/hello, world/i));
-    });
-  });
-
-  describe('title prop', () => {
-    it('should display text as Modal button children', () => {
-      render(<Modal title="Foo" width={320} height={480} />);
-      expect(screen.getByRole('button')).toHaveTextContent('Foo');
     });
   });
 });
