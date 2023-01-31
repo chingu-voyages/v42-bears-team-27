7da7ed1893f8dfcf456c3096f63e1ea0d8e426c9
Tarwat <t.uddin9121@gmail.com>
2023-01-31 23:02:22 +0000

    feat(modal): abstract Modal component
    Modal component replaced with Dialog component as root component for wrapping children.
    
    New components added:
    -> DialogContent - content of Dialog component
    -> DialogTrigger - trigger button to "open" Dialog component

--- packages/frontend/src/components/ui/Modal/Modal.tsx
+++ packages/frontend/src/components/ui/Modal/Modal.tsx
@@ -1,17 +1,12 @@
-import type {
-  HTMLAttributes,
-  RefAttributes,
-  ForwardRefExoticComponent,
-} from 'react';
+import type { RefAttributes, ForwardRefExoticComponent } from 'react';
 import { forwardRef } from 'react';
 import type { ThemeUIStyleObject } from 'theme-ui';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { MdClose } from 'react-icons/md';
 
-import { Button } from '../Button';
 import { IconButton } from '../IconButton';
 
-export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
+export interface DialogContentProps extends DialogPrimitive.DialogContentProps {
   /**
    * Title of the component
    */
@@ -31,71 +26,65 @@ export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
    * Height of the component
    */
   height: string | number;
-  btn?: React.ReactNode;
   sx?: ThemeUIStyleObject;
 }
 
-export const Modal: ForwardRefExoticComponent<
-ModalProps & RefAttributes<HTMLDivElement>
+const overlayStyles: ThemeUIStyleObject = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: 1000,
+  width: '100%',
+  height: '100%',
+  backdropFilter: 'blur(3px)',
+  bg: 'rgba(0, 0, 0, 0.6)',
+};
+
+const contentStyles: ThemeUIStyleObject = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  zIndex: 1001,
+  maxWidth: 880,
+  overflowY: 'auto',
+  py: 2,
+  px: 4,
+  bg: 'muted',
+  translate: '-50% -50%',
+  transition: 'opacity 700ms',
+};
+
+export const DialogContent: ForwardRefExoticComponent<
+DialogContentProps & RefAttributes<HTMLDivElement>
 > = forwardRef(
-  ({ children, title, description, width, height, btn, sx, ...rest }, ref) => (
-    <DialogPrimitive.Root>
-      <DialogPrimitive.Trigger asChild>
-        {btn || <Button>{title}</Button>}
-      </DialogPrimitive.Trigger>
-      <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay
-          sx={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            zIndex: 1000,
-            width: '100%',
-            height: '100%',
-            backdropFilter: 'blur(3px)',
-            bg: 'rgba(0, 0, 0, 0.6)',
-          }}
-        />
-        <DialogPrimitive.Content
-          ref={ref}
-          sx={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            zIndex: 1001,
-            maxWidth: 880,
-            overflowY: 'auto',
-            px: 4,
-            py: 2,
-            bg: 'muted',
-            translate: '-50% -50%',
-            transition: 'opacity 700ms',
-            width,
-            height,
-            ...sx,
-          }}
-          {...rest}
-        >
-          <DialogPrimitive.Title
-            sx={{ variant: 'text.h3', textAlign: 'center' }}
-          >
-            {title}
-          </DialogPrimitive.Title>
-          <DialogPrimitive.Description sx={{ variant: 'text.h4' }}>
-            {description}
-          </DialogPrimitive.Description>
-          {children}
-          <DialogPrimitive.Close asChild>
-            <IconButton sx={{ position: 'absolute', top: 4, right: 4 }}>
-              <MdClose size={32} />
-            </IconButton>
-          </DialogPrimitive.Close>
-        </DialogPrimitive.Content>
-      </DialogPrimitive.Portal>
-    </DialogPrimitive.Root>
+  ({ children, title, description, width, height, sx, ...rest }, ref) => (
+    <DialogPrimitive.Portal>
+      <DialogPrimitive.Overlay sx={overlayStyles} />
+      <DialogPrimitive.Content
+        ref={ref}
+        sx={{ ...contentStyles, width, height, ...sx }}
+        {...rest}
+      >
+        <DialogPrimitive.Title sx={{ variant: 'text.h3', textAlign: 'center' }}>
+          {title}
+        </DialogPrimitive.Title>
+        <DialogPrimitive.Description sx={{ variant: 'text.h4' }}>
+          {description}
+        </DialogPrimitive.Description>
+        {children}
+        <DialogPrimitive.Close asChild>
+          <IconButton sx={{ position: 'absolute', top: 4, right: 4 }}>
+            <MdClose size={32} />
+          </IconButton>
+        </DialogPrimitive.Close>
+      </DialogPrimitive.Content>
+    </DialogPrimitive.Portal>
   ),
 );
 
 if (process.env.NODE_ENV !== 'production') {
-  Modal.displayName = 'Modal';
+  DialogContent.displayName = 'DialogContent';
 }
+
+export const Dialog = DialogPrimitive.Root;
+export const DialogTrigger = DialogPrimitive.Trigger;
