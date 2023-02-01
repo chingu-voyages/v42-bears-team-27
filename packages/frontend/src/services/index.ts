7736d35b3e03df5b8b325211b27281b89f198792
Eloi-Perez <eloi104@hotmail.com>
2023-02-01 19:03:37 +0000

    feat: logout after 401

--- packages/frontend/src/services/index.ts
+++ packages/frontend/src/services/index.ts
@@ -6,7 +6,12 @@ export function fetcher(endpoint: string) {
     headers: {
       'Content-Type': 'application/json',
     },
-  }).then((res) => res.json());
+  }).then((res) => {
+    if (res.status === 401) {
+      // TODO Call logoutHandler function
+    }
+    return res.json();
+  });
 }
 
 export * from './classroom';
