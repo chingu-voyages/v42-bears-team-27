2deb32997cb3aab1939fa8f037cec07443bcb3cf
Tarwat <t.uddin9121@gmail.com>
2023-02-01 20:45:34 +0000

    refactor(pages): add major improvements and other necessary changes

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,62 +1,64 @@
 import type { ReactElement } from 'react';
 import { useEffect, useContext } from 'react';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-import MainHeader from 'components/home/MainHeader';
-import Layout from 'layouts/Layout';
-import type { IUserData } from 'interfaces';
-import { AuthContext } from 'store/auth';
+import Layout from 'src/layouts/Layout';
+import LandingHeader from 'src/components/landing/LandingHeader';
+import { AuthContext } from 'src/store/auth';
+import heroImg from 'public/hero.jpg';
+import type { NextPageWithLayout } from './_app';
 
-import heroImg from '../../public/hero.jpg';
-
-const Home = () => {
+const Landing: NextPageWithLayout = () => {
   const authCtx = useContext(AuthContext);
   const router = useRouter();
 
   useEffect(() => {
     if (authCtx?.isLoggedIn) {
-      router.replace(`/${(authCtx.user as IUserData).role}/dashboard/home`);
+      router.replace(`/${authCtx.role}/dashboard/home`);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authCtx?.isLoggedIn, router]);
+  }, [authCtx?.isLoggedIn]);
 
   return (
-    <div sx={{ pt: 5, px: 2 }}>
-      <div sx={{ textAlign: 'center', mb: 4 }}>
-        <Image
-          src={heroImg}
-          alt="Boy working on school work with laptop and headphones"
-          width={560}
-          height={352}
-        />
+    <>
+      <LandingHeader />
+      <div sx={{ pt: 5, px: 2 }}>
+        <div sx={{ textAlign: 'center', mb: 4 }}>
+          <Image
+            src={heroImg}
+            alt="Boy working on school work with laptop and headphones"
+            width={560}
+            height={352}
+            priority
+          />
+        </div>
+        <h2
+          sx={{
+            variant: 'text.h3',
+            width: 720,
+            mx: 'auto',
+            color: 'primary',
+            textAlign: 'center',
+          }}
+        >
+          An All-In-One Inclusive Learning Platform Built For Both Students and
+          Teachers
+        </h2>
       </div>
-      <h2
-        sx={{
-          variant: 'text.h3',
-          width: 720,
-          mx: 'auto',
-          color: 'primary',
-          textAlign: 'center',
-        }}
-      >
-        An All-In-One Inclusive Learning Platform Built For Both Students and
-        Teachers
-      </h2>
-    </div>
+    </>
   );
 };
 
-Home.getLayout = function getLayout(page: ReactElement) {
+Landing.getLayout = function getLayout(page: ReactElement) {
   return (
     <Layout
       title="RemoteClass"
       description="An all-in-one inclusive learning platform built for both students and teachers"
     >
-      <MainHeader />
       {page}
     </Layout>
   );
 };
 
-export default Home;
+export default Landing;
