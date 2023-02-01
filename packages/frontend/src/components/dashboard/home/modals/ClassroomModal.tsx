832cc3aa742dfc72f9626d244e8ef2aa4c5f5ea1
Tarwat <t.uddin9121@gmail.com>
2023-02-01 00:01:02 +0000

    style(classroom-modal): add minor improvements

--- packages/frontend/src/components/dashboard/home/modals/ClassroomModal.tsx
+++ packages/frontend/src/components/dashboard/home/modals/ClassroomModal.tsx
@@ -1,18 +1,23 @@
-import { Modal, Button } from 'components/ui';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from 'src/components/ui';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const ClassroomModal: React.FC<Props> = ({ children }) => (
-  <Modal
-    title="Classroom"
-    width="95%"
-    height="90vh"
-    btn={<Button variant="outlined">View Classroom</Button>}
-  >
-    {children}
-  </Modal>
+  <Dialog>
+    <DialogTrigger asChild>
+      <Button variant="outlined">View Classroom</Button>
+    </DialogTrigger>
+    <DialogContent title="Classroom" width="95%" height="90vh">
+      {children}
+    </DialogContent>
+  </Dialog>
 );
 
 export default ClassroomModal;
