617cf87ca85d50481f6b272498496f19e4505349
Henok Hailemariam <henokhm2@gmail.com>
2023-02-06 21:53:30 +0300

    refactor: save/remove socketIDs in DB on student/teacher login/logout

--- packages/backend/socket.js
+++ packages/backend/socket.js
@@ -1,6 +1,35 @@
 const { Server } = require('socket.io');
 
+const Student = require('./models/studentModel');
+const Teacher = require('./models/teacherModel');
+
 let io;
+
+async function addSocketIDsToDB(userData, socket) {
+  if (userData.role === 'teacher') {
+    await Teacher.findOneAndUpdate(
+      { _id: userData._id },
+      { socketID: socket.id },
+    );
+  }
+
+  if (userData.role === 'student') {
+    await Student.findOneAndUpdate(
+      { _id: userData._id },
+      { socketID: socket.id },
+    );
+  }
+}
+
+async function removeSocketIDsFromDB(userData) {
+  if (userData.role === 'teacher') {
+    await Teacher.findOneAndUpdate({ _id: userData._id }, { socketID: '' });
+  }
+
+  if (userData.role === 'student') {
+    await Student.findOneAndUpdate({ _id: userData._id }, { socketID: '' });
+  }
+}
 
 module.exports = {
   initializeSocketConnection: (httpServer, corsOptions) => {
@@ -11,11 +40,14 @@ module.exports = {
     io.on('connection', (socket) => {
       console.log(`user connected on socket with id ${socket.id}`);
 
-      socket.on('socket-created', () => console.log('socket created recieved'));
+      socket.on('user-logged-in', async (userData) => {
+        socket.emit('ack-user-logged-in');
+        addSocketIDsToDB(userData, socket);
+      });
 
-      socket.on('user-logged-in', (userData) => {
-        socket.emit('ack-user-signed-in');
-        console.log(`user with data ${JSON.stringify(userData)} signed in`);
+      socket.on('user-logged-out', async (userData) => {
+        socket.emit('ack-user-logged-out');
+        removeSocketIDsFromDB(userData);
       });
 
       socket.on('disconnect', () => {
