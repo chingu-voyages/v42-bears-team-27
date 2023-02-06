a24837b0db9e9bbbfb6fc0be261614a210c2b09c
Eloi-Perez <eloi104@hotmail.com>
2023-02-06 16:59:06 +0000

    feat: create student profile file

--- packages/frontend/src/components/dashboard/modals/index.ts
+++ packages/frontend/src/components/dashboard/modals/index.ts
@@ -1,3 +1,4 @@
 export { default as BroadcastModal } from './BroadcastModal';
 export { default as ClassroomModal } from './ClassroomModal';
 export { default as DirectMessageModal } from './DirectMessageModal';
+export { default as StudentProfileModal } from './StudentProfileModal';
