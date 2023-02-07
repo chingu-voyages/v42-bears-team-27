1b50d46d11fb7026717ec8b2b3665f7bf03d641a
Eloi-Perez <eloi104@hotmail.com>
2023-02-07 00:22:33 +0000

    feat: hide button if user is student

--- packages/frontend/src/components/dashboard/modals/ClassroomModal/StudentProfileModal.tsx
+++ packages/frontend/src/components/dashboard/modals/ClassroomModal/StudentProfileModal.tsx
@@ -1,37 +1,61 @@
-// import { useContext, useState } from 'react';
+import { useContext } from 'react';
 // import useSWR from 'swr';
 // import { MdAdd } from 'react-icons/md';
 
 // import { fetcher } from 'src/services';
 import { Button } from 'src/components/ui';
 import type { IStudent } from 'src/interfaces';
+import { AuthContext } from 'src/store/auth';
 
 type Props = {
   setForm: (form: string) => void;
   student?: IStudent | null;
 };
 
-const StudentProfileModal: React.FC<Props> = ({ setForm, student }) => (
-  <div
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: '2em',
-    }}
-  >
-    <p>Time Spent</p>
-    <p>Points Achieved</p>
-    <p>{JSON.stringify(student?.tasks)}</p>
-    <Button
-      onClick={() => setForm('directMessage')}
-      variant="outlined"
-      autoFocus
+const StudentProfileModal: React.FC<Props> = ({ setForm, student }) => {
+  const authCtx = useContext(AuthContext);
+
+  if (!authCtx || !student) {
+    return (
+      <p
+        sx={{
+          variant: 'text.h3',
+          position: 'absolute',
+          top: '40%',
+          left: '50%',
+          translate: '-50% -50%',
+        }}
+      >
+        Loading Profile...
+      </p>
+    );
+  }
+
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '2em',
+      }}
     >
-      Send Message
-    </Button>
-  </div>
-);
+      <p>Time Spent</p>
+      <p>Points Achieved</p>
+      <p>{JSON.stringify(student?.tasks)}</p>
+
+      {authCtx.role === 'teacher' && (
+        <Button
+          onClick={() => setForm('directMessage')}
+          variant="outlined"
+          autoFocus
+        >
+          Send Message
+        </Button>
+      )}
+    </div>
+  );
+};
 
 export default StudentProfileModal;
