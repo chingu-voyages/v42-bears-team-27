c642bfb5c742e68c20261277b4c5a7a948b9dc92
Tarwat <t.uddin9121@gmail.com>
2023-02-07 16:41:01 +0000

    feat(populatedb): update subjects with new properties: slug & imageUrl

--- packages/backend/populateDB.js
+++ packages/backend/populateDB.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 console.log(
-  'This script populates some test subjects to your database. Specified database as argument - e.g.: populateDB mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true',
+  'This script populates some test subjects to your database. Specified database as argument - e.g.: populateDB mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/remote_class?retryWrites=true',
 );
 
 // Get arguments passed on command line
@@ -26,8 +26,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const subjects = [];
 
-function subjectCreate(title, topics, cb) {
-  const subjectDetail = { title, topics };
+function subjectCreate(slug, title, imageUrl, topics, cb) {
+  const subjectDetail = { slug, title, imageUrl, topics };
   console.log(subjectDetail);
 
   const subject = new Subject(subjectDetail);
@@ -48,11 +48,13 @@ function createSubjects(cb) {
     [
       (callback) => {
         subjectCreate(
-          'english',
+          'computer-science',
+          'Computer Science',
+          'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
           [
             {
-              slug: 'punctuation',
-              title: 'Punctuation',
+              slug: 'booleans',
+              title: 'Booleans',
               types: ['lesson'],
             },
           ],
@@ -61,25 +63,14 @@ function createSubjects(cb) {
       },
       (callback) => {
         subjectCreate(
-          'mathematics',
+          'maths',
+          'Maths',
+          'https://images.unsplash.com/photo-1635372722656-389f87a941b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80',
           [
             {
               slug: 'indices',
               title: 'Indices',
               types: ['exercise'],
-            },
-          ],
-          callback,
-        );
-      },
-      (callback) => {
-        subjectCreate(
-          'history',
-          [
-            {
-              slug: 'cold-war',
-              title: 'Cold War',
-              types: ['test'],
             },
           ],
           callback,
