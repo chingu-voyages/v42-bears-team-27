c45bfe49f097e8ce3513097bf29639e6f48bba9b
Tarwat Uddin <91470221+TazDeCoder@users.noreply.github.com>
2023-02-09 23:03:03 +0000

    feat(hooks): add useElapsedTime custom hook (#172)

--- packages/frontend/src/hooks/index.ts
+++ packages/frontend/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useInput } from './use-input';
 export { default as useLocalstorageState } from './use-localstorage-state';
+export { default as useElapsedTime } from './use-elapsed-time';
