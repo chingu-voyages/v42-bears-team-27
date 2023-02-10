c2ae6288c2de306686fa6b69aa316b44b85cbf4b
Henok Hailemariam <henokhm2@gmail.com>
2023-02-10 21:51:09 +0300

    feat: add bar chart to student profile page

--- packages/frontend/src/pages/student/dashboard/profile.tsx
+++ packages/frontend/src/pages/student/dashboard/profile.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useContext } from 'react';
+import useSWR from 'swr';
 import { MdLogout } from 'react-icons/md';
 
+import { fetcher } from 'src/services';
 import AuthLayout from 'src/layouts/AuthLayout';
 import {
   AlertDialog,
@@ -9,11 +11,17 @@ import {
   Button,
 } from 'src/components/ui';
 import { StudentAppBar, StudentBottomNav } from 'src/components/dashboard/navs';
+import BarChart from 'src/components/dashboard/student/Profile';
 import { AuthContext } from 'src/store/auth';
+import { IStudentProfile } from 'src/interfaces';
 import type { NextPageWithLayout } from '../../_app';
 
 const Profile: NextPageWithLayout = () => {
   const authCtx = useContext(AuthContext);
+  const { data } = useSWR<IStudentProfile>(
+    `/api/v0/student/profile/${authCtx?.user?._id}`,
+    fetcher,
+  );
 
   return (
     <div
@@ -25,6 +33,34 @@ const Profile: NextPageWithLayout = () => {
       }}
     >
       <StudentAppBar />
+      <div>
+        <h1>Check out your awesome profile stats 🚀</h1>
+        {data && (
+          <div>
+            <div
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                py: 4,
+                color: 'primary',
+                bg: 'muted',
+                minHeight: '100vh',
+              }}
+            >
+              <BarChart
+                chartLabel="Time Spent in Minutes on each Subject"
+                dataFromAPI={data.timeSpent}
+              />
+              <BarChart
+                chartLabel="Points Gained in each Subject"
+                dataFromAPI={data.points}
+              />
+            </div>
+          </div>
+        )}
+      </div>
       <div sx={{ overflowY: 'auto' }}>
         <AlertDialog>
           <AlertDialogTrigger asChild>
