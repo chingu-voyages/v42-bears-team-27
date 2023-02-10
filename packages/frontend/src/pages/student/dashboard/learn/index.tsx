307c97f3535129e4742f7a4afa5ee6d51e632806
Tarwat <t.uddin9121@gmail.com>
2023-02-10 17:06:31 +0000

    feat(learn): add dashboard components

--- packages/frontend/src/pages/student/dashboard/learn/index.tsx
+++ packages/frontend/src/pages/student/dashboard/learn/index.tsx
@@ -4,6 +4,7 @@ import useSWR from 'swr';
 
 import AuthLayout from 'src/layouts/AuthLayout';
 import Loader from 'src/components/common/Loader';
+import { StudentAppBar, StudentBottomNav } from 'src/components/dashboard/navs';
 import ClassroomSubjects from 'src/components/dashboard/student/ClassroomSubjects';
 import type { ISubject } from 'src/interfaces';
 import { AuthContext } from 'src/store/auth';
@@ -19,17 +20,32 @@ const Learn: NextPageWithLayout = () => {
     error,
   } = useSWR<ISubject[]>('/api/v0/classroom/subjects', fetcher);
 
-  if (!authCtx || isLoading) {
+  if (isLoading) {
     return <Loader>Loading Dashboard...</Loader>;
   }
 
   if (error) {
     // Assuming any error when fetching data means that user cookies have expired,
     // therefore logout the user from the app since they're not authenticated
-    authCtx.onLogout();
+    authCtx?.onLogout();
   }
 
-  return <ClassroomSubjects subjects={subjectsData || []} />;
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100vh',
+      }}
+    >
+      <StudentAppBar />
+      <div sx={{ overflowY: 'auto' }}>
+        <ClassroomSubjects subjects={subjectsData || []} />
+      </div>
+      <StudentBottomNav />
+    </div>
+  );
 };
 
 Learn.getLayout = function getLayout(page: ReactElement) {
