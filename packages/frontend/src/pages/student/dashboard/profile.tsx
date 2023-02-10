c68c1f51fa8816798c939502f603c7536977281c
Tarwat Uddin <91470221+TazDeCoder@users.noreply.github.com>
2023-02-10 17:48:13 +0000

    Feature/student dashboard (#177)
    * feat(navs): add StudentNav component
    
    * style(student-nav): rename component to 'StudentAppBar'
    
    * feat(student-app-bar): add isolated component data fetching
    
    * feat(navs): add StudentBottomNav component
    
    * feat(student-bottom-nav): add styling improvement considerations to component
    
    * fix(student-bottom-nav): correct path of nav links
    
    * feat(home): add dashboard components and page-related components
    
    * feat(profile): add dashboard components
    
    * feat(lessons): add dashboard components
    
    * feat(learn): add dashboard components

--- packages/frontend/src/pages/student/dashboard/profile.tsx
+++ packages/frontend/src/pages/student/dashboard/profile.tsx
@@ -1,19 +1,59 @@
-import type { ReactElement } from 'react';
-import { useContext } from 'react';
+import { ReactElement, useContext } from 'react';
+import { MdLogout } from 'react-icons/md';
 
 import AuthLayout from 'src/layouts/AuthLayout';
-import Loader from 'src/components/common/Loader';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogTrigger,
+  Button,
+} from 'src/components/ui';
+import { StudentAppBar, StudentBottomNav } from 'src/components/dashboard/navs';
 import { AuthContext } from 'src/store/auth';
 import type { NextPageWithLayout } from '../../_app';
 
 const Profile: NextPageWithLayout = () => {
   const authCtx = useContext(AuthContext);
 
-  if (!authCtx) {
-    return <Loader>Loading Dashboard...</Loader>;
-  }
-
-  return <div>Profile</div>;
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100vh',
+      }}
+    >
+      <StudentAppBar />
+      <div sx={{ overflowY: 'auto' }}>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button sx={{ mx: 'auto' }} icon={<MdLogout />}>
+              Logout
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent
+            sx={{
+              textAlign: 'center',
+              py: 4,
+              '& button': {
+                mt: 4,
+              },
+              '& div': {
+                justifyContent: 'center !important',
+              },
+            }}
+            title="Are you sure you want to logout?"
+            description=""
+            width="32rem"
+            height="min-content"
+            onConfirm={() => authCtx?.onLogout()}
+          />
+        </AlertDialog>
+      </div>
+      <StudentBottomNav />
+    </div>
+  );
 };
 
 Profile.getLayout = function getLayout(page: ReactElement) {
