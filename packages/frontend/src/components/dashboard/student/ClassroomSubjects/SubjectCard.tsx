d3de1850c4ed09cf6e9e8894ec58e06b36b38ab7
Eloi-Perez <eloi104@hotmail.com>
2023-02-11 01:23:28 +0000

    feat: progress bar

--- packages/frontend/src/components/dashboard/student/ClassroomSubjects/SubjectCard.tsx
+++ packages/frontend/src/components/dashboard/student/ClassroomSubjects/SubjectCard.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
+import { useMemo } from 'react';
+import useSWR from 'swr';
+import stc from 'string-to-color';
 
+import { fetcher } from 'src/services';
 import { ButtonLink } from 'src/components/ui';
 import type { ISubject } from 'src/interfaces';
 
@@ -7,8 +11,33 @@ type Props = {
   subject: Omit<ISubject, '_id'>;
 };
 
+type PropsData = {
+  _id: string;
+  completed: boolean;
+  event: string;
+  taskID: any;
+};
+
 const SubjectCard: React.FC<Props> = ({ subject }) => {
   const { slug, title, imageUrl } = subject;
+
+  const {
+    data,
+    // isLoading,
+    // error,
+    // } = useSWR<IStudentTask[]>('/api/v0/student/tasks', fetcher,);
+  } = useSWR<PropsData[]>('/api/v0/student/tasks', fetcher);
+
+  const percentageProgress = useMemo(() => {
+    const filterData = data?.filter(
+      (task) => task.taskID.assignment.subject.slug === slug,
+    );
+    const completed = filterData?.filter((task) => task.completed).length;
+    const total = filterData?.length;
+    const percentageString =
+      completed && total ? `${(completed / total) * 100}%` : '0%';
+    return percentageString;
+  }, [data, slug]);
 
   return (
     <div
@@ -42,14 +71,40 @@ const SubjectCard: React.FC<Props> = ({ subject }) => {
         </h2>
       </div>
       <div
-        sx={{ display: 'flex', justifyContent: 'center', columnGap: 3, p: 5 }}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          mt: 5,
+          p: 5,
+        }}
       >
-        <ButtonLink href={`./learn/${slug}/lessons`} rounded={false}>
-          Lessons
-        </ButtonLink>
-        <ButtonLink href={`./learn/${slug}/exercises`} rounded={false}>
-          Exercises
-        </ButtonLink>
+        <div
+          sx={{
+            display: 'flex',
+            bg: 'white',
+            borderRadius: '8px',
+            overflow: 'hidden',
+            mb: 4,
+          }}
+        >
+          <div
+            sx={{
+              bg: stc(subject),
+              height: '100%',
+              width: percentageProgress,
+            }}
+          >
+            <p>&nbsp;</p>
+          </div>
+        </div>
+        <div sx={{ display: 'flex', justifyContent: 'center', columnGap: 3 }}>
+          <ButtonLink href={`./learn/${slug}/lessons`} rounded={false}>
+            Lessons
+          </ButtonLink>
+          <ButtonLink href={`./learn/${slug}/exercises`} rounded={false}>
+            Exercises
+          </ButtonLink>
+        </div>
       </div>
     </div>
   );
