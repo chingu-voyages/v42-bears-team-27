16eac584ece6406ee2c8bb024db7cf8370756b2e
Tarwat Uddin <91470221+TazDeCoder@users.noreply.github.com>
2023-02-12 00:07:46 +0000

    feat(hooks): add useUser hook (#187)
    Hook allows you to get data of the current logged-in user from api

--- packages/frontend/src/hooks/index.ts
+++ packages/frontend/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useInput } from './use-input';
 export { default as useLocalstorageState } from './use-localstorage-state';
 export { default as useElapsedTime } from './use-elapsed-time';
+export { default as useUser } from './use-user';
