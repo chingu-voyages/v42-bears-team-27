1f3ce5017afc151461c39cd058ef07068447ce05
Tarwat Uddin <91470221+TazDeCoder@users.noreply.github.com>
2023-02-12 17:54:31 +0000

    feat(theme): update color scheme (#204)

--- packages/frontend/src/theme.ts
+++ packages/frontend/src/theme.ts
@@ -4,9 +4,6 @@ import '@fontsource/nunito';
 import '@fontsource/roboto';
 
 const theme: Theme = {
-  config: {
-    useColorSchemeMediaQuery: false,
-  },
   fonts: {
     body: '"Nunito", sans-serif',
     heading: '"Roboto", sans-serif',
@@ -14,11 +11,12 @@ const theme: Theme = {
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#495464',
-    primaryShade: '#424c5a',
-    secondary: '#e8e8e8',
-    secondaryShade: '#d1d1d1',
+    primary: '#79a3c1',
+    primaryShade: '#6d93ae',
+    secondary: '#f0eeed',
+    secondaryShade: '#d8d6d5',
     muted: '#f4f4f2',
+    mutedShade: '#dcdcda',
     error: '#f44336',
     warning: '#ffa726',
     info: '#29b6f6',
@@ -28,12 +26,12 @@ const theme: Theme = {
       dark: {
         text: '#fff',
         background: '#000',
-        primary: '#171717',
-        primaryShade: '#151515',
-        secondary: '#444444',
-        secondaryShade: '#3d3d3d',
-        muted: '#ededed',
-        accent: '#da0037',
+        primary: '#30475E',
+        primaryShade: '#2b4055',
+        secondary: '#313131',
+        secondaryShade: '#313131',
+        muted: '#3f3f3f',
+        mutedShade: '#393939',
       },
     },
   },
